49260de76c40bafe9091b4a79e0ea3ba96835c72
Sofimiau <143454244+Sofimiau@users.noreply.github.com>
2023-08-28 17:23:53 -0400

    Update HelloApplication.java

--- src/main/java/dev/bfms/tankgame/HelloApplication.java
+++ src/main/java/dev/bfms/tankgame/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+    
         stage.show();
     }
 
