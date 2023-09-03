895e881d316f7b7c72e555eac6f3b30e289e3101
Francisco Carvajal <fraco_oxza@outlook.com>
2023-09-03 09:48:45 -0300

    docs(uml): the uml of the general project has been created
    A general layout has been made but there are still a lot of methods to be defined.

--- src/main/java/dev/bfms/tankgame/HelloApplication.java
+++ src/main/java/dev/bfms/tankgame/HelloApplication.java
@@ -7,17 +7,35 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+enum States {
+    DONE,
+    UNDONE,
+    FAIL,
+    DOING,
+}
+
+enum TypeOfCollision {
+    SOIL,
+    AIR,
+    HIT,
+}
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+
     
         stage.show();
     }
 
     public static void main(String[] args) {
+        var actualState = States.DONE;
+
+
+
         launch();
     }
 }
