e6f8e264625fd5d830fda3e032005bb8ae7cf168
Maca <mmino21@alumnos.utalca.cl>
2023-09-03 18:32:13 -0300

    Primer paso para dibujar

--- src/main/java/dev/bfms/tankgame/Terrain.java
+++ src/main/java/dev/bfms/tankgame/Terrain.java
@@ -1,5 +1,17 @@
 package dev.bfms.tankgame;
 
-public class Terrain {
 
+import javafx.scene.canvas.GraphicsContext;
+
+public class Terrain implements Drawable{
+
+    @Override
+    public void draw(GraphicsContext gc) {
+
+    }
+
+    @Override
+    public void erase(GraphicsContext gc) {
+
+    }
 }
