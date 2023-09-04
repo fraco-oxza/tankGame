4779ac295c4f2eff66232417a5836000194e6bb5
Francisco Carvajal <fraco_oxza@outlook.com>
2023-09-04 18:54:25 -0300

    code(app.py): classes were initialized so they can work

--- src/app.py
+++ src/app.py
@@ -1,1 +1,112 @@
-print("Hola Team")
+from abc import abstractmethod
+import math
+
+import pygame
+
+import constants
+
+
+class Drawable:
+    @abstractmethod
+    def draw(self, screen: pygame.surface.Surface) -> None:
+        pass
+
+    @abstractmethod
+    def erase(self, screen: pygame.surface.Surface) -> None:
+        pass
+
+
+class Terrain(Drawable):
+    ground_lines: list[int]
+
+    def __init__(self, mountains: int, valleys: int):
+        # Este es el constructor, aquí puedes hacer la lógica
+        # que cree las ground_lines
+        width = constants.WINDOWS_SIZE[0]
+        self.ground_lines = [30] * int(width / constants.TERRAIN_LINE_WIDTH)
+
+    def draw(self, screen: pygame.surface.Surface) -> None:
+        for i in range(len(self.ground_lines)):
+            pygame.draw.rect(
+                screen,
+                "green",
+                pygame.Rect(
+                    i * constants.TERRAIN_LINE_WIDTH,
+                ),
+            )
+
+    def erase(self, screen: pygame.surface.Surface) -> None:
+        pass
+
+
+class Cannonball(Drawable):
+    position: pygame.Vector2
+    velocity: pygame.Vector2
+
+    def __init__(self, position: pygame.Vector2, velocity: pygame.Vector2):
+        self.position = position
+        self.velocity = velocity
+
+    def tick(self, dt: float):
+        pass
+
+    def draw(self, screen: pygame.surface.Surface) -> None:
+        pass
+
+    def erase(self, screen: pygame.surface.Surface) -> None:
+        pass
+
+
+class Player:
+    name: str
+    points: float
+
+    def __init__(self, name: str, points: float):
+        self.name = name
+        self.points = points
+
+
+class Tank(Drawable):
+    player: Player
+    color: pygame.Color
+    position: pygame.Vector2
+
+    def draw(self, screen: pygame.surface.Surface) -> None:
+        pass
+
+    def erase(self, screen: pygame.surface.Surface) -> None:
+        pass
+
+
+class TankGame:
+    terrain: Terrain
+    tanks: list[Tank]
+    cannonball: None | Cannonball
+    screen: pygame.Surface
+
+    def __init__(self) -> None:
+        self.terrain = Terrain(2, 2)  # TODO: Crear una constante
+
+        pygame.init()
+        self.screen = pygame.display.set_mode(constants.WINDOWS_SIZE)
+        self.clock = pygame.time.Clock()
+
+        # TODO: Inicializar tanques y asi
+
+    def start(self) -> None:
+        running = True
+
+        self.terrain.draw(self.screen)
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
