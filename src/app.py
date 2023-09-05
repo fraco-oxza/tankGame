9103fc5866eaa0c097f247b395d4bc4432f86236
Francisco Carvajal <fraco_oxza@outlook.com>
2023-09-04 22:02:37 -0300

    code: some constants have been added and the background color has been changed

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-import math
+from random import randint, random
 
 import pygame
 
@@ -23,7 +23,9 @@ class Terrain(Drawable):
         # Este es el constructor, aquí puedes hacer la lógica
         # que cree las ground_lines
         width = constants.WINDOWS_SIZE[0]
-        self.ground_lines = [30] * int(width / constants.TERRAIN_LINE_WIDTH)
+        self.ground_lines = [constants.SEA_LEVEL] * int(
+            width / constants.TERRAIN_LINE_WIDTH
+        )
 
     def draw(self, screen: pygame.surface.Surface) -> None:
         for i in range(len(self.ground_lines)):
@@ -52,7 +54,8 @@ class Cannonball(Drawable):
         self.velocity = velocity
 
     def tick(self, dt: float):
-        pass
+        self.position += self.velocity * dt
+        self.velocity[1] += constants.GRAVITY * dt
 
     def draw(self, screen: pygame.surface.Surface) -> None:
         pass
@@ -75,6 +78,11 @@ class Tank(Drawable):
     color: pygame.Color
     position: pygame.Vector2
 
+    def __init__(self, color: pygame.Color, position: pygame.Vector2):
+        self.color = color
+        self.position = position
+        # player no lo usaremos en las primeras
+
     def draw(self, screen: pygame.surface.Surface) -> None:
         pass
 
@@ -89,23 +97,26 @@ class TankGame:
     screen: pygame.Surface
 
     def __init__(self) -> None:
-        self.terrain = Terrain(2, 2)  # TODO: Crear una constante
+        self.terrain = Terrain(constants.MOUNTAINS, constants.VALLEYS)
 
         pygame.init()
         self.screen = pygame.display.set_mode(constants.WINDOWS_SIZE)
         self.clock = pygame.time.Clock()
+        self.cannonball = None
 
         # TODO: Inicializar tanques y asi
 
     def start(self) -> None:
         running = True
 
-        self.terrain.draw(self.screen)
-
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            self.screen.fill(constants.SKY_COLOR)
+            self.terrain.draw(self.screen)
+            self.clock.tick(constants.FPS)
 
 
 def main():
