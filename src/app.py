d4df87376ac709b0faf0d06c63386c3adec3b9b0
Maca <mmino21@alumnos.utalca.cl>
2023-09-10 11:55:59 -0300

    Principal Mountains in process

--- src/app.py
+++ src/app.py
@@ -73,7 +73,12 @@ class Terrain(Drawable, Collidable):
             lista.append(constants.SEA_LEVEL+160)
         return lista
 
-
+    def increaseMountain(self,lista:list,indexInicio:int, indexFinal:int):
+        for i in range(indexInicio,indexFinal):
+            h=lista[i-1]
+            h = h + 1
+            lista.append(h)
+        return lista
     def __init__(self, mountains: int, valleys: int):
         self.ground_lines = self.completeList()
 
