7757aeaf27cfae430d24ceda5b075b9de5fc4cf3
Francisco Carvajal <fraco_oxza@outlook.com>
2023-09-25 14:19:44 -0300

    code: release mode

--- src/app.py
+++ src/app.py
@@ -73,7 +73,7 @@ class Background(Drawable):
     def __init__(self):
         """Initialize the class by loading the images and creating the snowflakes."""
         image_size = pygame.Vector2(
-            constants.WINDOWS_SIZE[0], (9 / 16) * constants.WINDOWS_SIZE[0]
+            constants.WINDOWS_SIZE[0], (1.0/constants.ASPECT_RATIO) * constants.WINDOWS_SIZE[0]
         )
         self.sky_image = pygame.transform.scale(
             pygame.image.load(resource_path("images/sky.jpg")), image_size
