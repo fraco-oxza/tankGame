137a83cf0313371246cb3c62a25c65e3fbfbf4f4
Francisco Carvajal <fraco_oxza@outlook.com>
2023-10-25 15:11:28 -0300

    fix: added files

---
+++ src/exit_requested.py
@@ -0,0 +1,2 @@
+class ExitRequested(Exception):
+    pass
---
+++ src/inputs.py
@@ -0,0 +1,24 @@
+import pygame
+
+from exit_requested import ExitRequested
+
+
+def check_running():
+    """
+    This method checks if the player has sent the signal to close the
+    window and stops the execution if this is the case, it is also
+    responsible for cleaning any position that has been left unused.
+    """
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            raise ExitRequested
+
+
+def run_until_exit(func):
+    def wrapper(*args, **kargs):
+        try:
+            return func(*args, **kargs)
+        except ExitRequested:
+            raise ExitRequested
+
+    return wrapper
