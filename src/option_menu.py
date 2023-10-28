6c09f9fc8c6722b8e470c3975727d661d14b3659
Maca <mmino21@alumnos.utalca.cl>
2023-10-27 23:45:25 -0300

    Menú de opciones casi terminado, solo falta hacer el botón de guardar cambios, deje todo lo que ingreso el usuario registrado y puede que se cambie el color de la letra (en realidad el color de la letra, es un color que se escogió en base al color de los botones)

--- src/option_menu.py
+++ src/option_menu.py
@@ -19,20 +19,22 @@ class OptionMenu:
     sobre: Optional[int]
 
     def __init__(self, screen: pygame.Surface):
-        self.button_reset_position = pygame.Vector2(instance.windows_size[0] / 25, instance.windows_size[1] / 15)
+        self.secondary_buttons = pygame.Vector2(instance.windows_size[0] / 25, instance.windows_size[1] / 15)
+        self.principal_button_size = pygame.Vector2(180, 210)
         self.font = font_cache["Roboto.ttf", int(instance.windows_size[0] / 51.2)]
-        self.button_color1 = "#2E3440"
-        self.button_color2 = "#2E3440"
-        self.button_color3 = "#2E3440"
-        self.button_color4 = "#2E3440"
-        self.button_color5 = "#2E3440"
-        self.button_color6 = "#2E3440"
-        self.button_color7 = "#2E3440"
-        self.button_color8 = "#2E3440"
-        self.button_color9 = "#2E3440"
-        self.button_color10 = "#2E3440"
-        self.button_color11 = "#2E3440"
-        self.hover_botton_color = "#3b4252"
+        self.button_color1 = "#C6614D"
+        self.button_color2 = "#C6614D"
+        self.button_color3 = "#C6614D"
+        self.button_color4 = "#C6614D"
+        self.button_color5 = "#C6614D"
+        self.button_color6 = "#C6614D"
+        self.button_color7 = "#C6614D"
+        self.button_color8 = "#C6614D"
+        self.button_color9 = "#C6614D"
+        self.button_color10 = "#C6614D"
+        self.button_color11 = "#A4715C"
+        self.hover_botton_color = "#AD564A"
+        self.hover_botton_color_continue = "#9F705C"
         self.screen = screen
         self.sobre = None
         self.clock = pygame.time.Clock()
@@ -46,7 +48,7 @@ class OptionMenu:
         self.quantity_players = 2
         self.quantity_bots = 0
         self.quantity_rounds = 1
-        self.environment_effects = ["Nada", "Gravedad", "Viento", "Gravedad y Viento"]
+        self.environment_effects = ["Ninguno", "Gravedad", "Viento", "Gravedad y Viento"]
         self.index_environment_effects = 0
         self.screen_resolution = ["800 X 800", "1366 X 768", "1600 x 900", "1280 X 720", "1920 X 1080"]
         self.index_screen_resolution = 0
@@ -55,14 +57,7 @@ class OptionMenu:
         while True:
             check_running()
             self.screen.blit(self.image, self.sky_rect.topleft)
-            transparency = 150
-            rect_surface = pygame.Surface(
-                (instance.windows_size[0], instance.windows_size[1])
-            )
-            rect_surface.fill("#000000")
-            rect_surface.set_alpha(transparency)
-            rect_x1, rect_y1 = (0, 0)
-            self.screen.blit(rect_surface, (rect_x1, rect_y1))
+
             self.screen.blit(self.paint_left(1), (370, 130))
             self.screen.blit(self.paint_left(2), (370, 225))
             self.screen.blit(self.paint_left(3), (370, 315))
@@ -73,6 +68,7 @@ class OptionMenu:
             self.screen.blit(self.paint_right(8), (840, 315))
             self.screen.blit(self.paint_right(9), (840, 405))
             self.screen.blit(self.paint_right(10), (840, 495))
+            self.screen.blit(self.principal_button(11), (1018, 325))
             msj = self.font.render("Tamaño de la Pantalla", True, "#ffffff")
             self.screen.blit(msj
                              ,
@@ -110,28 +106,28 @@ class OptionMenu:
                              )
             mouse = pygame.Vector2(pygame.mouse.get_pos())
             self.handle_input(mouse)
-            msj = self.font.render(f"{self.quantity_players}", True, "#560083")
+            msj = self.font.render(f"{self.quantity_players}", True, "#74BAB1")
             self.screen.blit(msj
                              ,
                              (
                                  620, 245,
                              ),
                              )
-            msj = self.font.render(f"{self.quantity_bots}", True, "#560083")
+            msj = self.font.render(f"{self.quantity_bots}", True, "#74BAB1")
             self.screen.blit(msj
                              ,
                              (
                                  620, 335,
                              ),
                              )
-            msj = self.font.render(f"{self.quantity_rounds}", True, "#560083")
+            msj = self.font.render(f"{self.quantity_rounds}", True, "#74BAB1")
             self.screen.blit(msj
                              ,
                              (
                                  620, 425,
                              ),
                              )
-            msj = self.font.render(f"{self.environment_effects[self.index_environment_effects]}", True, "#560083")
+            msj = self.font.render(f"{self.environment_effects[self.index_environment_effects]}", True, "#74BAB1")
             if self.index_environment_effects < len(self.environment_effects) - 1:
                 self.screen.blit(msj
                                  ,
@@ -146,7 +142,7 @@ class OptionMenu:
                                      540, 515,
                                  ),
                                  )
-            msj = self.font.render(f"{self.screen_resolution[self.index_screen_resolution]}", True, "#560083")
+            msj = self.font.render(f"{self.screen_resolution[self.index_screen_resolution]}", True, "#74BAB1")
             if self.index_screen_resolution == 0:
                 self.screen.blit(msj
                                  ,
@@ -173,101 +169,101 @@ class OptionMenu:
             130,
         )
         if button_left_1[0] < mouse.x < (
-                button_left_1[0] + self.button_reset_position[0]
+                button_left_1[0] + self.secondary_buttons[0]
         ) and button_left_1[1] < mouse.y < (
-                button_left_1[1] + self.button_reset_position[1]
+                button_left_1[1] + self.secondary_buttons[1]
         ):
             self.button_color1 = self.hover_botton_color
             self.sobre = 1
         else:
-            self.button_color1 = "#2E3440"
+            self.button_color1 = "#C6614D"
         button_left_2 = (370, 225)
         if button_left_2[0] < mouse.x < (
-                button_left_2[0] + self.button_reset_position[0]
-        ) and button_left_2[1] < mouse.y < (button_left_2[1] + self.button_reset_position[1]):
+                button_left_2[0] + self.secondary_buttons[0]
+        ) and button_left_2[1] < mouse.y < (button_left_2[1] + self.secondary_buttons[1]):
             self.button_color2 = self.hover_botton_color
             self.sobre = 2
         else:
-            self.button_color2 = "#2E3440"
+            self.button_color2 = "#C6614D"
         button_left_3 = (370, 315)
         if button_left_3[0] < mouse.x < (
-                button_left_3[0] + self.button_reset_position[0]
-        ) and button_left_3[1] < mouse.y < (button_left_3[1] + self.button_reset_position[1]):
+                button_left_3[0] + self.secondary_buttons[0]
+        ) and button_left_3[1] < mouse.y < (button_left_3[1] + self.secondary_buttons[1]):
             self.button_color3 = self.hover_botton_color
             self.sobre = 3
         else:
-            self.button_color3 = "#2E3440"
+            self.button_color3 = "#C6614D"
         button_left_4 = (370, 405)
         if button_left_4[0] < mouse.x < (
-                button_left_4[0] + self.button_reset_position[0]
-        ) and button_left_4[1] < mouse.y < (button_left_4[1] + self.button_reset_position[1]):
+                button_left_4[0] + self.secondary_buttons[0]
+        ) and button_left_4[1] < mouse.y < (button_left_4[1] + self.secondary_buttons[1]):
             self.button_color4 = self.hover_botton_color
             self.sobre = 4
         else:
-            self.button_color4 = "#2E3440"
+            self.button_color4 = "#C6614D"
         button_left_5 = (370, 495)
         if button_left_5[0] < mouse.x < (
-                button_left_5[0] + self.button_reset_position[0]
-        ) and button_left_5[1] < mouse.y < (button_left_5[1] + self.button_reset_position[1]):
+                button_left_5[0] + self.secondary_buttons[0]
+        ) and button_left_5[1] < mouse.y < (button_left_5[1] + self.secondary_buttons[1]):
             self.button_color5 = self.hover_botton_color
             self.sobre = 5
         else:
-            self.button_color5 = "#2E3440"
+            self.button_color5 = "#C6614D"
         button_left_6 = (840, 130)
         if button_left_6[0] < mouse.x < (
-                button_left_6[0] + self.button_reset_position[0]
-        ) and button_left_6[1] < mouse.y < (button_left_6[1] + self.button_reset_position[1]):
+                button_left_6[0] + self.secondary_buttons[0]
+        ) and button_left_6[1] < mouse.y < (button_left_6[1] + self.secondary_buttons[1]):
             self.button_color6 = self.hover_botton_color
             self.sobre = 6
         else:
-            self.button_color6 = "#2E3440"
+            self.button_color6 = "#C6614D"
         button_left_7 = (840, 225)
         if button_left_7[0] < mouse.x < (
-                button_left_7[0] + self.button_reset_position[0]
-        ) and button_left_7[1] < mouse.y < (button_left_7[1] + self.button_reset_position[1]):
+                button_left_7[0] + self.secondary_buttons[0]
+        ) and button_left_7[1] < mouse.y < (button_left_7[1] + self.secondary_buttons[1]):
             self.button_color7 = self.hover_botton_color
             self.sobre = 7
         else:
-            self.button_color7 = "#2E3440"
+            self.button_color7 = "#C6614D"
         button_left_8 = (840, 315)
         if button_left_8[0] < mouse.x < (
-                button_left_8[0] + self.button_reset_position[0]
-        ) and button_left_8[1] < mouse.y < (button_left_8[1] + self.button_reset_position[1]):
+                button_left_8[0] + self.secondary_buttons[0]
+        ) and button_left_8[1] < mouse.y < (button_left_8[1] + self.secondary_buttons[1]):
             self.button_color8 = self.hover_botton_color
             self.sobre = 8
         else:
-            self.button_color8 = "#2E3440"
+            self.button_color8 = "#C6614D"
         button_left_9 = (840, 405)
         if button_left_9[0] < mouse.x < (
-                button_left_9[0] + self.button_reset_position[0]
-        ) and button_left_9[1] < mouse.y < (button_left_9[1] + self.button_reset_position[1]):
+                button_left_9[0] + self.secondary_buttons[0]
+        ) and button_left_9[1] < mouse.y < (button_left_9[1] + self.secondary_buttons[1]):
             self.button_color9 = self.hover_botton_color
             self.sobre = 9
         else:
-            self.button_color9 = "#2E3440"
+            self.button_color9 = "#C6614D"
         button_left_10 = (840, 495)
         if button_left_10[0] < mouse.x < (
-                button_left_10[0] + self.button_reset_position[0]
-        ) and button_left_10[1] < mouse.y < (button_left_10[1] + self.button_reset_position[1]):
+                button_left_10[0] + self.secondary_buttons[0]
+        ) and button_left_10[1] < mouse.y < (button_left_10[1] + self.secondary_buttons[1]):
             self.button_color10 = self.hover_botton_color
             self.sobre = 10
         else:
-            self.button_color10 = "#2E3440"
-        button_left_11 = (0, 0)
+            self.button_color10 = "#C6614D"
+        button_left_11 = (1018, 325)
         if button_left_11[0] < mouse.x < (
-                button_left_11[0] + self.button_reset_position[0]
-        ) and button_left_11[1] < mouse.y < (button_left_11[1] + self.button_reset_position[1]):
-            self.button_color11 = self.hover_botton_color
+                button_left_11[0] + self.principal_button_size[0]
+        ) and button_left_11[1] < mouse.y < (button_left_11[1] + self.principal_button_size[1]):
+            self.button_color11 = self.hover_botton_color_continue
             self.sobre = 11
         else:
-            self.button_color11 = "#2E3440"
+            self.button_color11 = "#A4715C"
 
     def paint_left(self, index: int):
         """
         Function responsible for creating the surface that represents the button,
         in addition to writing the message of each button in the center of each surface
         """
-        sf = pygame.Surface(self.button_reset_position)
+        sf = pygame.Surface(self.secondary_buttons)
         if index == 1:
             sf.fill(self.button_color1)
         elif index == 2:
@@ -287,7 +283,7 @@ class OptionMenu:
         Function responsible for creating the surface that represents the button,
         in addition to writing the message of each button in the center of each surface
         """
-        sf = pygame.Surface(self.button_reset_position)
+        sf = pygame.Surface(self.secondary_buttons)
         if index == 6:
             sf.fill(self.button_color6)
         elif index == 7:
@@ -298,12 +294,25 @@ class OptionMenu:
             sf.fill(self.button_color9)
         elif index == 10:
             sf.fill(self.button_color10)
-        elif index == 11:
-            sf.fill(self.button_color11)
         pygame.draw.polygon(sf, "#ffffff", [(10, 10), (40, 25), (10, 40)])
 
         return sf
 
+    def principal_button(self, index: int):
+        sf = pygame.Surface(self.principal_button_size)
+        if index == 11:
+            sf.fill(self.button_color11)
+        box_size = sf.get_size()
+        end = self.font.render("Continuar", True, "#ffffff")
+        box_pos = ((box_size[0] - box_size[0]) / 3, box_size[1] / 4)
+        sf.blit(
+            end,
+            (
+                box_pos[0] + box_size[0] / 2 - end.get_size()[0] / 2,
+                box_pos[1] / 0.7,
+            ),
+        )
+        return sf
     def selection(self):
         if self.sobre == 1:
             if self.index_screen_resolution > 0:
